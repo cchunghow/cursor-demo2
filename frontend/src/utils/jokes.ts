9ad5befef8fa409754a15ad695b207ee84f35ebb
chunghow <cchunghow@gmail.com>
2025-06-16 17:27:51 +0800

    comit 2

--- frontend/src/utils/jokes.ts
+++ frontend/src/utils/jokes.ts
@@ -1,5 +1,6 @@
 export async function getDailyJoke() {
-  const res = await fetch('https://official-joke-api.appspot.com/random_joke', {
+  //const res = await fetch('https://official-joke-api.appspot.com/random_joke', {
+  const res = await fetch('http://127.0.0.1:4000/jokes/random', {
     cache: 'no-store'
     //next: { revalidate: 300 } 
   });
